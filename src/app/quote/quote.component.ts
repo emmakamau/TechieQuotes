27be7db48f3123d15c9862dae7f6983b22fe2d30
EmmaK <emmaculatewkamau@gmail.com>
2022-04-09 08:40:13 +0300

    Add heading and add double quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,7 +9,7 @@ import { Quote } from '../quote'
 export class QuoteComponent implements OnInit {
 
     quotes:Quote[] = [
-      new Quote(1,'Let us go invent tomorrow instead of worrying about what happened yesterday.','Steve Jobs','Emma','Inspire',new Date(2020,5,23),23,4),
+      new Quote(1,'Let us go invent tomorrow instead of worrying about what happened yesterday.Let us go invent tomorrow instead of worrying about what happened yesterday.','Steve Jobs','Emma','Inspire',new Date(2020,5,23),23,4),
       new Quote(1,'Never let a computer know you are in a hurry.','Author Unknown','Kamau','Humor',new Date(2021,4,21),6,7),
       new Quote(1,'Let us go invent tomorrow instead of worrying about what happened yesterday.','Steve Jobs','Emma','Inspire',new Date(2020,5,23),23,4),
       new Quote(1,'Never let a computer know you are in a hurry.','Author Unknown','Kamau','Humor',new Date(2021,4,21),6,7),
