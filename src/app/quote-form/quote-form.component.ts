ea35e01f7861256ba6be3a4df476d1639a4802b4
EmmaK <emmaculatewkamau@gmail.com>
2022-04-09 12:01:56 +0300

    Add form module to app and submit quote function

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Quote } from '../quote'
 
 @Component({
   selector: 'app-quote-form',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+
+  submitQuote(){
+    this.addQuote.emit(this.newQuote);
+      }
 
   constructor() { }
 
