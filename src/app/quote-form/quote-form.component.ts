9d26ee0de9238d1d9bab5a9a3fb79bebc0ebe1fe
EmmaK <emmaculatewkamau@gmail.com>
2022-04-10 09:47:37 +0300

    Disable submit form for empty form

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -12,10 +12,12 @@ export class QuoteFormComponent implements OnInit {
   newQuote = new Quote(0,"","","","",new Date(),0,0);
   @Output() addQuote = new EventEmitter<Quote>();
 
-  submitQuote(quoteForm: NgForm){
+  //Emit data to the parent component quote
+  submitQuote(){
     this.addQuote.emit(this.newQuote); 
       }
 
+  //On click of btn the form data is cleared    
   clearQuoteForm(quoteForm: NgForm){
     quoteForm.reset()
   }
